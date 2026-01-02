1860007b49158992a265d19d0bbf2bbf79adbf69
kxk254 <konno.kenji@gmail.com>
2026-01-02 18:03:42 +0900

    11

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ let foo = 1;
 foo = 2;
 
 console.log(foo);
+
+let tdo = 1;
+
+const questions = ["あなたは誰", "Hello", "日本の総理はだれ"];
+
+for (let i = 0; i < questions.length; i++) {
+  console.log("インデックス" + questions[i]);
+}
