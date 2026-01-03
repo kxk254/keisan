15dfdf9919766fe2db5a58aee8c340152606fcf3
kxk254 <konno.kenji@gmail.com>
2026-01-03 18:18:09 +0900

    12.6

--- reactproject/react-tutorial-youtube/src/App.js
+++ reactproject/react-tutorial-youtube/src/App.js
@@ -2,6 +2,7 @@
 // import './App.css';
 import {useState, useRef} from 'react';
 import TodoList from './TodoList' ;
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
@@ -15,18 +16,25 @@ function App() {
     // ここに新しいタスクを追加する処理を書く
     const name = todoNameRef.current.value;
     setTodos((prevTodos) => {
-      return [...prevTodos, {id: "1", name: name, completed: false}];
+      return [...prevTodos, {id: uuidv4(), name: name, completed: false}];
     });
     todoNameRef.current.value = null;
   };
 
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  }
+
   return (
     <>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input type="text" ref={todoNameRef} />
       <button onClick={(handleAddTodo)}>タスクを追加</button>
       <button>完了したタスクの削除</button>
-      <div>残りのタスク数:0</div>
+      <div>残りのタスク数:{todos.filter(todo => !todo.completed).length}</div>
     </>
   );
 
